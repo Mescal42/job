d28415da84360fefa89dc389d14dfd2337dc161f
Mescal <Mescal@mbp-de-mescal.home>
2020-11-05 19:45:39 +0100

    correc

--- int main(void).c
+++ int main(void).c
@@ -1,5 +1,29 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int ft_strlen(char *str)
+{
+    int i;
+    
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return(i);
+}
+
 int main(void)
 {
-    
+    int  i;
+    char str[] = "bi bop a lula"; 
 
+    i = 0;
+    i = ft_strlen(str);
+    ft_putchar(i);
+    return (0);
 }
